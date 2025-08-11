14eb2ded23131972eb38c7130812c5c546590f99
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2025-08-11 11:35:33 -0300

    Add files via upload

---
+++ carro.java
@@ -0,0 +1,21 @@
+package Teste;
+
+public class carro extends veiculo {
+
+   int portas = 4;
+
+   public void exibir_informaçoes() {
+      System.out.println("Marca " + marca);
+      System.out.println("Ano " + ano);
+      System.out.println("Portas " + portas);
+      Buzinar();
+
+
+
+
+   }
+
+
+
+}
+
---
+++ herança.java
@@ -0,0 +1,19 @@
+package Teste;
+
+public class herança{
+public static void main(String[] args) {
+    
+    
+    carro carro1 = new carro();
+
+    carro1.marca = "Toyota";
+    carro1.ano = 2022;
+    carro1.portas = 4;
+
+    carro1.Buzinar();
+    carro1.exibir_informaçoes();
+
+
+
+}
+}
