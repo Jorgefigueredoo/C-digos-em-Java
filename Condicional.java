b0add0852eb348b2bc2ae09b86fd6626678589eb
Jorge Figueredo <jorgefigueredo@gmail.com>
2025-08-13 16:01:12 -0300

    Novo arquivo

---
+++ Condicional.java
@@ -0,0 +1,17 @@
+
+
+public class Condicional {
+    public static void main(String[] args) {
+        int x = 5;
+
+        System.out.println("Bom dia");
+
+
+        if (x < 5){
+         System.out.println("Bom Tarde");
+        }
+        
+
+        System.out.println("Bom Noite");
+    }
+}
