09e4d32268aa8f46d885dec9d0ecf929a4f53770
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2025-08-16 10:16:01 -0300

    Funções em Java

---
+++ Funcao/sintaxe.java
@@ -0,0 +1,43 @@
+package Funcao;
+
+import java.util.Scanner;
+
+public class sintaxe {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe três numeros :");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int maior = max(a, b, c);
+
+        showResult(maior);
+
+        sc.close();
+    }
+
+    public static int max(int x, int y, int z){
+        int aux;
+        if(x > y && x >z){
+            aux = x;
+        } else if (y> z){
+            aux = y;
+        } else{
+            aux = z;
+        }
+        return aux;
+    }
+
+    public static void showResult(int valor){
+        System.out.println("Maior = " + valor);
+    }
+
+
+
+
+}
+
+
