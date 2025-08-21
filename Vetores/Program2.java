137c19586335dbe2f4632f385b76fcd929f26b06
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2025-08-21 18:10:39 -0300

    vetores

---
+++ Vetores/Program2.java
@@ -0,0 +1,31 @@
+package Vetores;
+
+import java.util.Scanner;
+
+public class Program2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Product[] vect = new Product[n];
+
+        for (int i = 0; i < n; i++) {
+            sc.nextLine();
+            String name = sc.nextLine();
+            double price = sc.nextDouble();
+            vect[i] = new Product(name, price);
+        }
+
+        double soma = 0;
+        for (int i = 0; i < n; i++) {
+            soma += vect[i].getPrice();
+        }
+
+        double media = soma / n;
+
+        System.out.println("Preço médio: " + media);
+
+        sc.close();
+    }
+}
