4455d169db69d676e642e2482cf4d21af6447d6b
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2025-08-25 17:37:57 -0300

    Exercicio concluido

--- Matrizes/Exercicio.java
+++ Matrizes/Exercicio.java
@@ -14,5 +14,20 @@ public class Exercicio {
                 mat[i][j] = sc.nextInt();
             }
         }
+        System.out.println("Main diagonal: ");
+        for (int i = 0; i < n; i++) {
+            System.out.println(mat[i][i] + "");
+        }
+        System.out.println();
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] < 0) {
+                    count++;
+                }
+            }
+        }
+        System.out.println("Negative numbers: " + count);
     }
 }
