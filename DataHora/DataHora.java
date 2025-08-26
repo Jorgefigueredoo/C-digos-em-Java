27bf5710ea88d4e5045fba2fa007319f54fe2ae4
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2025-08-26 15:58:10 -0300

    Date Time Formater

--- DataHora/DataHora.java
+++ DataHora/DataHora.java
@@ -3,10 +3,14 @@ package DataHora;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DataHora {
 
     public static void main(String[] args) {
+
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
         LocalDate d01 = LocalDate.now();
         LocalDateTime d02 = LocalDateTime.now();
@@ -17,6 +21,9 @@ public class DataHora {
         Instant d06 = Instant.parse("2025-08-26T15:38:45Z");
         Instant d07 = Instant.parse("2025-08-26T15:38:45-03:00");
 
+        LocalDate d08 = LocalDate.parse("26/08/2005", fmt1);
+        LocalDateTime d09 = LocalDateTime.parse("26/08/2005 15:38", fmt2);
+
         System.out.println("d01 =  " + d01.toString());
         System.out.println("d02 =  " + d02.toString());
         System.out.println("d03 = " + d03.toString());
@@ -24,5 +31,7 @@ public class DataHora {
         System.out.println("d05 = " + d05.toString());
         System.out.println("d06 = " + d06.toString());
         System.out.println("d07 = " + d07.toString());
+        System.out.println("d08 = " + d08.toString());
+        System.out.println("d09 = " + d09.toString());
     }
 }
