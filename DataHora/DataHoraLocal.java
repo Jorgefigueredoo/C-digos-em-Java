e0697a60e18411c3855cafbba758864a5bd277b9
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2025-08-26 17:28:25 -0300

    Get

--- DataHora/DataHoraLocal.java
+++ DataHora/DataHoraLocal.java
@@ -10,11 +10,21 @@ public class DataHoraLocal {
 
         LocalDate d04 = LocalDate.parse("2025-08-26");
         LocalDateTime d05 = LocalDateTime.parse("2025-08-26T15:38:45");
-        Instant d06 = Instant.parse("2025-08-26T15:38:45Z");
+        Instant d06 = Instant.parse("2025-08-26T01:30:45Z");
 
-        for (String s : ZoneId.getAvailableZoneIds()) {
-            System.out.println(s);
-        }
+        LocalDate r1 = LocalDate.ofInstant(d06, ZoneId.systemDefault());
+        LocalDate r2 = LocalDate.ofInstant(d06, ZoneId.of("Portugal"));
+        LocalDateTime r3 = LocalDateTime.ofInstant(d06, ZoneId.systemDefault());
+        LocalDateTime r4 = LocalDateTime.ofInstant(d06, ZoneId.of("Portugal"));
+
+        System.out.println("R1 = " + r1);
+        System.out.println("R2 = " + r2);
+        System.out.println("R3 = " + r3);
+        System.out.println("R4 = " + r4);
+
+        System.out.println("d04 dia = " + d04.getDayOfMonth());
+        System.out.println("d04 dia = " + d04.getMonthValue());
+        System.out.println("d04 dia = " + d04.getYear());
 
     }
 }
