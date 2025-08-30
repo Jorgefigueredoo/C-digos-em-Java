5c4838f3c9f2871e9a0a58cf85104fcd989c9cf4
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2025-08-30 09:49:55 -0300

    Worker

--- Composiçao/Program.java
+++ Composiçao/Program.java
@@ -1,12 +1,16 @@
 package Composiçao;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Program {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
         Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
         System.out.println("Enter departament's name: ");
         String departamentName = sc.nextLine();
@@ -25,6 +29,18 @@ public class Program {
                 new Departament(departamentName));
 
         System.out.println("How many contracts to this worker? ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i < n; i++) {
+            System.out.println("Enter Contract #" + i + "data");
+            System.out.println("Date (DD/MM/YYYY)");
+            Date contratcDate = sdf.parse(sc.next());
+            System.out.println("Value por hour: ");
+            double valuePerHour = sc.nextDouble();
+            int hours = sc.nextInt();
+            HourContratc contratc = new HourContratc(contratcDate, valuePerHour, hours);
+            worker.addContract(contratc);
+        }
 
         sc.close();
     }
