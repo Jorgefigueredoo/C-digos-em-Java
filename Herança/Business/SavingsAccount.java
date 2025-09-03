947fd84a4a03afb2b8137c48ce6abd56b2664fa0
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2025-09-03 15:57:16 -0300

    Save Program

---
+++ Herança/Business/SavingsAccount.java
@@ -0,0 +1,27 @@
+package Herança.Business;
+
+public class SavingsAccount extends Account {
+
+    private Double interstRate;
+
+    public SavingsAccount() {
+        super();
+    }
+
+    public SavingsAccount(Integer number, String holder, Double balance, Double interstRate) {
+        super(number, holder, balance);
+        this.interstRate = interstRate;
+    }
+
+    public Double getInterstRate() {
+        return interstRate;
+    }
+
+    public void setInterstRate(Double interstRate) {
+        this.interstRate = interstRate;
+    }
+
+    public void updateBalance() {
+        balance += balance * interstRate;
+    }
+}
