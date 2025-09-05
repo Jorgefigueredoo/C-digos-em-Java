21aeb2c310d026f0819ab3a632b0c27128ab11a1
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2025-09-05 15:56:32 -0300

    Classe abstrata

--- Herança/Business/Program.java
+++ Herança/Business/Program.java
@@ -1,16 +1,29 @@
 package Herança.Business;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program {
 
     public static void main(String[] args) {
 
-        Account x = new Account(1020, "Alex", 1000.0);
-        Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+        Account acc1 = new Account(1001, "Alex", 1000.0);
+        Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
+        Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
 
-        x.withdraw(50);
-        y.withdraw(50);
+        List<Account> list = new ArrayList<>();
 
-        System.out.println(x.getBalance());
-        System.out.println(y.getBalance());
+        list.add(new SavingsAccount(1001, "Alex", 500.0, 0.01));
+        list.add(new BusinessAccount(1002, "Maria", 1000.0, 400.0));
+        list.add(new SavingsAccount(1004, "Bob", 300.0, 0.01));
+        list.add(new BusinessAccount(1005, "Anna", 500.0, 500.0));
+
+        double sum = 0.0;
+        for (Account acc : list) {
+            sum += acc.getBalance();
+        }
+
+        System.out.printf("Total Balance: %.2f%n", sum);
+
     }
 }
