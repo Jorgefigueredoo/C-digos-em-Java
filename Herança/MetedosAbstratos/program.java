30324e2634cb7f2f2955cefeb5572f208e9ca618
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2025-09-09 15:39:35 -0300

    Exercicio

--- Herança/MetedosAbstratos/program.java
+++ Herança/MetedosAbstratos/program.java
@@ -1,5 +1,28 @@
 package Herança.MetedosAbstratos;
 
+import java.util.Scanner;
+
 public class program {
 
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of shapes");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Shape #" + i + "Data: ");
+            System.out.print("Rectangle or Circle (r/c)? ");
+            char ch = sc.next().charAt(0);
+            System.out.print("Color (BLACK/BLUE/RED): ");
+            Color color = Color.valueOf(sc.next());
+            if (ch == 'r') {
+                System.out.print("Widht: ");
+                double width = sc.nextDouble();
+                System.out.print("Height: ");
+                double height = sc.nextDouble();
+            }
+        }
+    }
 }
